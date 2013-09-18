b101d4d2226bdf03dac10421143dec704040b841
Alexander Saprykin <itallium@yandex.ru>
2013-09-18 22:48:35 +0900

    Обновление CheckWinner.java от 18.09.2013г. Реализована проверка победителя.

--- src/CheckWinner.java
+++ src/CheckWinner.java
@@ -1,6 +1,26 @@
 public class CheckWinner {
 
-    public static void check() {
-        SeaWars.winner = ' ';
+    static boolean humanWinner, compWinner;
+
+    public static boolean isWinner() {
+        humanWinner = true;
+        compWinner = true;
+
+        for(int i = 0; i < 10; i++) {
+
+            if(HumanMove.myBattleships.get(i).health != 0) {
+                humanWinner = false;
+            }
+
+            if(CompMove.enemyBattleships.get(i).health != 0) {
+                compWinner = false;
+            }
+        }
+
+        if(humanWinner || compWinner) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
