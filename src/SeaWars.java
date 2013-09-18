43132fcdb157650cce2e145b312a0ac995b2129d
Alexander Saprykin <itallium@yandex.ru>
2013-09-18 22:53:48 +0900

    Обновление SeaWars.java от 18.09.2013г. Реализован механизм подготовки полей к выводу (удаление служебных символов). Реализован алгоритм ходов (первый ход делает человек). Добавлена проверка победителя и вывод результатов боя.

--- src/SeaWars.java
+++ src/SeaWars.java
@@ -56,17 +56,36 @@ public class SeaWars {
 
     public static char winner = ' ';
 
+    public static void prepareFieldsToGame() {
+        for(int i = 0; i <= 9; i++) {
+            for(int j = 0; j <= 9; j++) {
+                if(enemyField[i][j] == '%') enemyField[i][j] = ' ';
+                if(myField[i][j] == '%') myField[i][j] = ' ';
+            }
+        }
+    }
+
     public static void printFields() {
         System.out.println("       Ваш флот:            Флот врага:");
         System.out.println("  0 1 2 3 4 5 6 7 8 9     0 1 2 3 4 5 6 7 8 9");
         for (int i = 0; i <= 9; i++) {
             System.out.print(i + "|");
             for (int j = 0; j <= 9; j++) {
-                System.out.print(myField[i][j] + "|");
+                if(myField[i][j] == '0' || myField[i][j] == '1' || myField[i][j] == '2' || myField[i][j] == '3' || myField[i][j] == '4' ||
+                        myField[i][j] == '5' || myField[i][j] == '6' || myField[i][j] == '7' || myField[i][j] == '8' || myField[i][j] == '9') {
+                    System.out.print("@|");
+                } else {
+                    System.out.print(myField[i][j] + "|");
+                }
             }
             System.out.print("  " + i + "|");
             for (int j = 0; j <= 9; j++) {
-                System.out.print(enemyFieldForMy[i][j] + "|");
+                if(enemyField[i][j] == '0' || enemyField[i][j] == '1' || enemyField[i][j] == '2' || enemyField[i][j] == '3' || enemyField[i][j] == '4' ||
+                        enemyField[i][j] == '5' || enemyField[i][j] == '6' || enemyField[i][j] == '7' || enemyField[i][j] == '8' || enemyField[i][j] == '9') {
+                    System.out.print("@|");
+                } else {
+                    System.out.print(enemyFieldForMy[i][j] + "|");
+                }
             }
             System.out.print("\n");
         }
@@ -74,19 +93,27 @@ public class SeaWars {
 
     public static void main(String[] args) throws IOException {
 
-        HumanMove.stand();
-        CompMove.stand();
+        HumanMove.move();
+        CompMove.move();
+        prepareFieldsToGame();
         while(true) {
             printFields();
-            HumanMove.move();
-            if(winner != ' ') {
+            HumanMove.shoot();
+            if(CheckWinner.isWinner()) {
                 break;
             }
-            CompMove.move();
-            if(winner != ' ') {
+            CompMove.shoot();
+            if(CheckWinner.isWinner()) {
                 break;
             }
         }
-
+        if(CheckWinner.humanWinner) {
+            printFields();
+            System.out.println("YOU WIN!!!");
+        }
+        if(CheckWinner.compWinner) {
+            printFields();
+            System.out.println("YOU LOSE!!!");
+        }
     }
 }
